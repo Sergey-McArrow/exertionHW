743b9f77c8a084eb163163e2e8d56ac4ba5c27be
Сергей Макаров <sergey.mcarrow@gmail.com>
2018-03-28 10:40:41 +0300

    array

--- exertionHW/Program.cs
+++ exertionHW/Program.cs
@@ -8,8 +8,35 @@ namespace exertionHW
 {
     class Program
     {
+        public static void ExeptionRandomArray()
+        {
+            Random rnd = new Random();
+            int length = rnd.Next(0, 20);
+            int fillnumbers = rnd.Next(0, 100);
+            byte[] arrayExeption = new byte[length];
+            try
+            {
+                for (int i = 0; i < fillnumbers; i++)
+                {
+                    int numbers = rnd.Next(256, 500);
+                    byte numberstemp = (byte) numbers;
+                    arrayExeption[i] =  numberstemp;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            foreach (var item in arrayExeption)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
         static void Main(string[] args)
         {
+            ExeptionRandomArray();
+            Console.ReadLine();
         }
     }
 }
